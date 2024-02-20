0f712ba11d7bfdfde2ccd331344d205512e8daf7
Akanksha <akanksha012btit21@igdtuw.ac.in>
2024-02-21 02:03:56 +0530

    Add ComplaintCard component and update dependencies

--- frontend/src/pages/CommunityWall/CommunityWall.jsx
+++ frontend/src/pages/CommunityWall/CommunityWall.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
+import ComplaintCard from '../../components/Card/ComplaintCard'
 
 export const CommunityWall = () => {
   return (
     <div>
         <Navbar />
-        
+        <div className='m-3'>
+          <ComplaintCard />
+        </div>
         <Footer/>
     </div>
   )
